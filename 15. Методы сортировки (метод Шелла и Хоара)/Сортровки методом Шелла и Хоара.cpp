2a6b00ef736c4624e1e3d4ead3025b9a35b923df
Пономарев Егор <egorghb@users.noreply.github.com>
2021-03-09 10:26:47 +0500

    15

---
+++ 15. Методы сортировки (метод Шелла и Хоара)/Сортровки методом Шелла и Хоара.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+
+struct timetable {
+    string starting_st;
+    string end_st;
+};
+
+timetable* arr;
+
+void create() {
+    cout << "Введите количество рейсов: ";
+    cin >> n;
+    arr = new timetable[n];
+    cin.ignore();
+    for (int i = 0; i < n; i++) {
+
+        cout << "Введите станцию отправления " << i + 1 << " рейса: ";
+        getline(cin, arr[i].starting_st);
+
+        cout << "Введите конечную станцию " << i + 1 << " рейса: ";
+        getline(cin, arr[i].end_st);
+
+        cout << endl;
+    }
+    cout << "\nРасписание успешно составлено!\n";
+}
+
+void print() {
+    if (n == 0) {
+        cout << "Пусто.\n";
+    }
+    else {
+        cout << endl;
+        cout << "Ваше расписание:\n\n";
+        for (int i = 0; i < n; i++) {
+            cout << i + 1 << " рейс:\n";
+            cout << "Станция отправления: " << arr[i].starting_st << endl;
+            cout << "Конечная станция: " << arr[i].end_st << endl;
+            cout << endl;
+        }
+    }
+}
+
+void shell_sort() {
+
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    system("chcp 1251");
+    
+    create();
+    print();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
